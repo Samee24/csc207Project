557597ec2688735e2d9ea51df1ae27ece2390352
Samee24 <sameescouser24@gmail.com>
2014-11-19 20:23:51 -0600

    comment right braces

--- src/sportsScheduler/Game.java
+++ src/sportsScheduler/Game.java
@@ -15,11 +15,11 @@ public class Game
   {
     this.homeTeam = home;
     this.awayTeam = away;
-  }
+  }// Game(Team home, Team away)
 
   public String toString()
   {
     String returnString = this.awayTeam.name + " at " + this.homeTeam.name;
     return returnString;
-  }
-}
+  }// toString()
+}// class
